347b7696b8d9f99d6e5a79216a8deebf370a504e
Bjorn Commers <BjornCommers@users.noreply.github.com>
2023-06-08 21:52:04 -0400

    Making a SQL Translator
    Made translator using OpenAI's API

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+import constants
+
+app = Flask(__name__)
+
+openai.api_key = constants.openai_api_key
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt=generate_prompt(animal),
+          temperature=0,
+          max_tokens=150,
+          top_p=1.0,
+          frequency_penalty=0.0,
+          presence_penalty=0.0,
+          stop=["#", ";"]
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(animal):
+    return """### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### {}\n
+""".format(
+        animal
+    )
